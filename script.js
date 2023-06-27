80ee2ed1a31c9faf08fdd35438b259b80de800dc
cphung913 <chasephung13@gmail.com>
2023-06-26 21:38:36 -0700

    created slider + functionality

--- script.js
+++ script.js
@@ -2,21 +2,30 @@ const board = document.querySelector(".board");
 const row = document.createElement("div");
 const tile = document.createElement("div");
 
-let set = 16;
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = (() => {
+    let value = inputSlider.value;
+    slideValue.textContent = value;
+    createTiles(value);
+})
 
 row.classList.add("row");
 tile.classList.add("tile");
 
 function createTiles(num) {
+    board.innerHTML = "";
+    const tempRow = row.cloneNode();
+    
     for(let i = 0; i < num; i++) {
         const temp = tile.cloneNode();
-        row.append(temp);
+        tempRow.append(temp);
     }
 
     for(let i = 0; i < num; i++) {
-        const temp = row.cloneNode(true);
+        const temp = tempRow.cloneNode(true);
         board.append(temp);
     }
 }
 
-createTiles(set);
+createTiles(16);
