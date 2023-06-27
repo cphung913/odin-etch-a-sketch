300c63ec9929b38c4e0e2cb19936a8edf7c2d72e
cphung913 <chasephung13@gmail.com>
2023-06-26 22:33:17 -0700

    added drawing and clear functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const board = document.querySelector(".board");
 const row = document.createElement("div");
 const tile = document.createElement("div");
+const clearButton = document.querySelector("button");
+let isMouseDown = false;
 
 const slideValue = document.querySelector("span");
 const inputSlider = document.querySelector("input");
@@ -10,8 +12,22 @@ inputSlider.oninput = (() => {
     createTiles(value);
 })
 
+document.body.addEventListener("click", toggleMouseDown);
+clearButton.addEventListener("click", clearTiles);
+
 row.classList.add("row");
 tile.classList.add("tile");
+
+board.addEventListener('mouseover',  changeColor);
+
+function changeColor(e) {
+    if (!isMouseDown) return;
+    e.target.style.backgroundColor = "black";
+}
+
+function toggleMouseDown() {
+    isMouseDown = !isMouseDown;
+}
 
 function createTiles(num) {
     board.innerHTML = "";
@@ -20,12 +36,23 @@ function createTiles(num) {
     for(let i = 0; i < num; i++) {
         const temp = tile.cloneNode();
         tempRow.append(temp);
+        temp.addEventListener('mouseover',  changeColor);
     }
 
     for(let i = 0; i < num; i++) {
         const temp = tempRow.cloneNode(true);
         board.append(temp);
     }
+    clearTiles();
+    isMouseDown = true;
+}
+
+function clearTiles() {
+    const tiles = document.querySelectorAll(".tile");
+    tiles.forEach(element => {
+        element.style.backgroundColor = "white";
+    });
+    toggleMouseDown();
 }
 
 createTiles(16);
